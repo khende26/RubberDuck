cbf045392cd53d3e7958365fcbc2756d351f55ed
Karri Wright <karriwright@Karris-MacBook-Pro.local>
2022-02-14 15:46:58 -0500

    Add initial repo

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int Ducks = 345;
+        if(Ducks < 200) {
+            System.out.println("Ask a Better Question");
+    } else if (Ducks > 500) {
+        System.out.println("Simplify your Question");
+    } else {
+        System.out.println("Great Job");
+    }
+    }}
+
